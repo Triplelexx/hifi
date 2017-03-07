84e28bdcf1ee0f6b07514da251ff90f812b11f09
Triplelexx <mojolexx@gmail.com>
2017-03-07 22:57:01 +0000

    redo tetherballStick scripts
    This seems a better approach in case of possible duplication of the
    entity, various aesthetic changes.

--- scripts/tutorials/createTetherballStick.js
+++ scripts/tutorials/createTetherballStick.js
@@ -13,127 +13,65 @@
 // Distributed under the Apache License, Version 2.0.
 // See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 
-var STICK_SCRIPT_URL =  Script.resolvePath("./entity_scripts/tetherballStick.js?v=" + Date.now());
+var STICK_SCRIPT_URL = Script.resolvePath("./entity_scripts/tetherballStick.js?v=" + Date.now());
 var STICK_MODEL_URL = "http://hifi-content.s3.amazonaws.com/caitlyn/production/raveStick/newRaveStick2.fbx";
-var COLLISION_SOUND_URL = "http://hifi-production.s3.amazonaws.com/tutorials/pistol/drop.wav";
-var NULL_UUID = "{00000000-0000-0000-0000-000000000000}";
 
 var avatarOrientation = MyAvatar.orientation;
 avatarOrientation = Quat.safeEulerAngles(avatarOrientation);
 avatarOrientation.x = 0;
 avatarOrientation = Quat.fromVec3Degrees(avatarOrientation);
-var startPosition = Vec3.sum(MyAvatar.getHeadPosition(), Vec3.multiply(2, Quat.getFront(avatarOrientation)));
+var startPosition = Vec3.sum(MyAvatar.getRightPalmPosition(), Vec3.multiply(1, Quat.getFront(avatarOrientation)));
 
 var STICK_PROPERTIES = {
-  type: 'Model',
-  name: "Tetherball Stick",
-  modelURL: STICK_MODEL_URL,
-  position: startPosition,
-  dimensions: {
-    x: 0.0651,
-    y: 0.0651,
-    z: 0.5270
-  },
-  script: STICK_SCRIPT_URL,
-  color: {
-    red: 200,
-    green: 0,
-    blue: 20
-  },
-  shapeType: 'box',
-  dynamic: false,
-  lifetime: 3600,
-  userData: JSON.stringify({
-    userID: MyAvatar.sessionUUID,
-    ballID: NULL_UUID,
-    actionID: NULL_UUID,
-    lineID: NULL_UUID,
-    grabbableKey: {
-      invertSolidWhileHeld: true
+    type: 'Model',
+    name: "tetherballStick Stick",
+    modelURL: STICK_MODEL_URL,
+    position: startPosition,
+    rotation: Quat.fromPitchYawRollDegrees(0.0, -90.0, 0.0),
+    dimensions: {
+        x: 0.0651,
+        y: 0.0651,
+        z: 0.5270
     },
-    wearable: {
-      joints: {
-        RightHand: [{
-          x: 0.07079616189002991,
-          y: 0.20177987217903137,
-          z: 0.06374628841876984
-        }, {
-          x: -0.5863648653030396,
-          y: -0.46007341146469116,
-          z: 0.46949487924575806,
-          w: -0.4733745753765106
-        }],
-        LeftHand: [{
-          x: 0.1802254319190979,
-          y: 0.13442856073379517,
-          z: 0.08504903316497803
-        }, {
-          x: 0.2198076844215393,
-          y: -0.7377811074256897,
-          z: 0.2780133783817291,
-          w: 0.574519157409668
-        }]
-      }
-    }
-  })
+    script: STICK_SCRIPT_URL,
+    color: {
+        red: 200,
+        green: 0,
+        blue: 20
+    },
+    shapeType: 'box',
+    dynamic: false,
+    lifetime: 3600,
+    userData: JSON.stringify({
+        grabbableKey: {
+            invertSolidWhileHeld: true
+        },
+        wearable: {
+            joints: {
+                RightHand: [{
+                    x: 0.07079616189002991,
+                    y: 0.20177987217903137,
+                    z: 0.06374628841876984
+                }, {
+                    x: -0.5863648653030396,
+                    y: -0.46007341146469116,
+                    z: 0.46949487924575806,
+                    w: -0.4733745753765106
+                }],
+                LeftHand: [{
+                    x: 0.1802254319190979,
+                    y: 0.13442856073379517,
+                    z: 0.08504903316497803
+                }, {
+                    x: 0.2198076844215393,
+                    y: -0.7377811074256897,
+                    z: 0.2780133783817291,
+                    w: 0.574519157409668
+                }]
+            }
+        }
+    })
 };
 
-var stickID = Entities.addEntity(STICK_PROPERTIES);
-
-var ballID = Entities.addEntity({
-    type: "Model",
-    modelURL: "http://hifi-content.s3.amazonaws.com/Examples%20Content/production/marblecollection/Star.fbx",
-    name: "Tetherball",
-    shapeType: "Sphere",
-    position: startPosition,
-    collisionSoundURL: COLLISION_SOUND_URL,
-    dimensions: {
-        x: 0.2,
-        y: 0.2,
-        z: 0.2
-    },
-    gravity: {
-        x: 0.0,
-        y: -9.8,
-        z: 0.0
-    },
-    damping: 0.3,
-    angularDamping: 0.1,
-    density: 300,
-    restitution: 0.5,
-    dynamic: true,
-    collidesWith: "static,dynamic,kinematic,otherAvatar,"
-});
-
-var lineID = Entities.addEntity({
-    type: "PolyLine",
-    name: "Tetherball Line",
-    color: { red: 0, green: 120, blue: 250 },
-    textures: "https://hifi-public.s3.amazonaws.com/alan/Particles/Particle-Sprite-Smoke-1.png",
-    position: startPosition,
-    dimensions: { x: 10, y: 10, z: 10 }
-});
-
-// the values set for the action here don't really matter, they'll be overridden on use
-var offsetActionID = Entities.addAction("offset", ballID, {
-    pointToOffsetFrom: startPosition,
-    linearDistance: 0.1,
-    linearTimeScale: 0.1
-});
-
-// now the other items have been created the references can be added to the userData
-var stickProps = Entities.getEntityProperties(stickID);
-if (stickProps.userData) {
-    try {
-        var stickData = JSON.parse(stickProps.userData);
-        stickData.actionID = offsetActionID;
-        stickData.ballID = ballID;
-        stickData.lineID = lineID;
-        Entities.editEntity(stickID, {
-            userData: JSON.stringify(stickData)
-        });
-    } catch (e) {
-    }
-}
-
+Entities.addEntity(STICK_PROPERTIES);
 Script.stop();
--- scripts/tutorials/entity_scripts/tetherballStick.js
+++ scripts/tutorials/entity_scripts/tetherballStick.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /*jslint nomen: true, vars: true, plusplus: true*/
-/*global Entities, Vec3, MyAvatar, Camera, Script, print*/
+/*global Entities, Vec3, MyAvatar, Controller, Camera, Script, print*/
 
 // tetherballStick.js
 //
@@ -18,9 +18,22 @@
 
     var NULL_UUID = "{00000000-0000-0000-0000-000000000000}";
     var REPOSITION_INTERVAL = 30; // time in ms
-    var TETHER_LENGTH = 0.5;
-    var TETHER_TIMESCALE = 0.01;
-    var TETHER_WIDTH = 0.02;
+    var ENTITY_CHECK_INTERVAL = 5000;
+    var LINE_WIDTH = 0.02;
+    var BALL_SIZE = 0.175;
+    var BALL_DAMPING = 0.3;
+    var BALL_ANGULAR_DAMPING = 0.1;
+    var BALL_RESTITUTION = 0.4;
+    var BALL_DENSITY = 300;
+    var ACTION_DISTANCE = 0.5;
+    var ACTION_DISTANCE_INCREMENT = 0.005;
+    var ACTION_TIMESCALE = 0.01;
+    var ACTION_TAG = "tetherballStick Action";
+    var BALL_NAME = "tetherballStick Ball";
+    var LINE_NAME = "tetherballStick Line";
+    var COLLISION_SOUND_URL = "http://public.highfidelity.io/sounds/Footsteps/FootstepW3Left-12db.wav";
+    var EQUIP_SOUND_URL = "http://hifi-public.s3.amazonaws.com/sounds/color_busters/powerup.wav";
+    var EQUIP_SOUND_VOLUME = 0.2;
 
     tetherballStick = function() {
         _this = this;
@@ -30,99 +43,204 @@
     tetherballStick.prototype = {
         equipped: false,
         lastReposition: 0,
+        lastEntityCheck: 0,
+        lineLength: 0,
+        userID: NULL_UUID,
+        ballID: NULL_UUID,
+        lineID: NULL_UUID,
+        actionID: NULL_UUID,
+        EQUIP_SOUND: undefined,
 
         preload: function(entityID) {
             this.entityID = entityID;
-            // I have been using update and grab scripts to test without hand controllers
-            // Script.update.connect(this.update);
+            this.EQUIP_SOUND = SoundCache.getSound(EQUIP_SOUND_URL);
+            Script.update.connect(this.update);
         },
 
         unload: function() {
-            // Script.update.disconnect(this.update);
+            Script.update.disconnect(this.update);
         },
 
         update: function(dt) {
-            var dataProps = Entities.getEntityProperties(_this.entityID);
-            if (dataProps.userData) {
-                try {
-                    var data = JSON.parse(dataProps.userData);
-                    if(data.userID == MyAvatar.sessionUUID && _this.hasBall()) {
-                        // updating every tick seems excessive, so repositioning is throttled here
-                        if(Date.now() - _this.lastReposition >= REPOSITION_INTERVAL) {
-                            _this.lastReposition = Date.now();
-                            _this.repositionTether();
-                        }
-                    }
-                } catch (e) {
-                }
+            // _this during update due to loss of scope
+            if (_this.lastCheckForEntity >= ENTITY_CHECK_INTERVAL) {
+                _this.checkForEntities();
+                _this.lastCheckForEntity = 0;
+            } else {
+                _this.lastCheckForEntity += dt;
             }
+
+            var isUser = _this.userID == MyAvatar.sessionUUID; // only the user should control the line
+            if (_this.equipped && _this.lineLength < ACTION_DISTANCE && isUser) { // increase line after startEquip
+                _this.lineLength += ACTION_DISTANCE_INCREMENT;
+            } else if (!_this.equipped && _this.lineLength > 0 && isUser) { // reduce lineLength after releaseEquip
+                _this.lineLength -= ACTION_DISTANCE_INCREMENT;
+            } else if (!_this.equipped && _this.lineLength <= 0 && isUser) {
+                _this.deleteLine();
+                _this.userID = NULL_UUID;
+            }
+        },
+
+        checkForEntities: function() {
+            if (!this.hasBall()) {
+                this.createBall();
+            }
+            if (!this.hasAction()) {
+                this.createAction();
+            }
+        },
+
+        playEquipSound: function() {
+            var stickProps = Entities.getEntityProperties(this.entityID);
+            var EQUIP_SOUND_OPTIONS = {
+                volume: EQUIP_SOUND_VOLUME,
+                loop: false,
+                position: stickProps.position
+            };
+            Audio.playSound(this.EQUIP_SOUND, EQUIP_SOUND_OPTIONS);
         },
 
         startEquip: function(id, params) {
+            var hand = params[0];
+            Controller.triggerShortHapticPulse(1, hand);
             this.equipped = true;
+            this.userID = MyAvatar.sessionUUID;
+            this.createLine();
+            this.playEquipSound();
         },
 
         continueEquip: function(id, params) {
-            if (!this.equipped || !this.hasBall()) {
+            if (!this.equipped || !this.hasRequiredComponents()) {
                 return;
             }
             // updating every tick seems excessive, so repositioning is throttled here
-            if(Date.now() - this.lastReposition >= REPOSITION_INTERVAL) {
+            if (Date.now() - this.lastReposition >= REPOSITION_INTERVAL) {
                 this.lastReposition = Date.now();
-                this.repositionTether();
+                this.repositionAction();
             }
         },
 
         releaseEquip: function(id, params) {
             this.equipped = false;
+            var stickProps = Entities.getEntityProperties(this.entityID);
+            this.playEquipSound();
+        },
+
+        createLine: function() {
+            var stickProps = Entities.getEntityProperties(this.entityID);
+            this.lineID = Entities.addEntity({
+                type: "PolyLine",
+                name: LINE_NAME,
+                color: {
+                    red: 0,
+                    green: 120,
+                    blue: 250
+                },
+                textures: "https://hifi-public.s3.amazonaws.com/alan/Particles/Particle-Sprite-Smoke-1.png",
+                position: stickProps.position,
+                dimensions: {
+                    x: 10,
+                    y: 10,
+                    z: 10
+                }
+            });
+            this.lineLength = 0;
+        },
+
+        deleteLine: function() {
+            Entities.deleteEntity(this.lineID);
+            this.lineID = NULL_UUID;
+        },
+
+        hasLine: function() {
+            var lineProps = Entities.getEntityProperties(this.lineID);
+            return lineProps.name == LINE_NAME;
+        },
+
+        createBall: function() {
+            var stickProps = Entities.getEntityProperties(this.entityID);
+            this.ballID = Entities.addEntity({
+                type: "Model",
+                modelURL: "http://hifi-content.s3.amazonaws.com/Examples%20Content/production/marblecollection/Star.fbx",
+                name: BALL_NAME,
+                shapeType: "Sphere",
+                position: stickProps.position,
+                lifetime: 3600,
+                collisionSoundURL: COLLISION_SOUND_URL,
+                dimensions: {
+                    x: BALL_SIZE,
+                    y: BALL_SIZE,
+                    z: BALL_SIZE
+                },
+                gravity: {
+                    x: 0.0,
+                    y: -9.8,
+                    z: 0.0
+                },
+                damping: BALL_DAMPING,
+                angularDamping: BALL_ANGULAR_DAMPING,
+                density: BALL_DENSITY,
+                restitution: BALL_RESTITUTION,
+                dynamic: true,
+                collidesWith: "static,dynamic,otherAvatar,"
+            });
         },
 
         hasBall: function() {
-            var dataProps = Entities.getEntityProperties(this.entityID);
-            if (dataProps.userData) {
-                try {
-                    var data = JSON.parse(dataProps.userData);
-                    return data.ballID != NULL_UUID && data.ballID.length == NULL_UUID.length;
-                } catch (e) {
-                    return false;
-                }
-            }
+            var ballProps = Entities.getEntityProperties(this.ballID);
+            return ballProps.name == BALL_NAME;
         },
 
-        repositionTether: function() {
+        createAction: function() {
             var stickProps = Entities.getEntityProperties(this.entityID);
-            if (stickProps.userData) {
-                try {
-                    var stickData = JSON.parse(stickProps.userData);
+            var actionID = Entities.addAction("offset", this.ballID, {
+                pointToOffsetFrom: stickProps.position,
+                linearDistance: 0,
+                tag: ACTION_TAG,
+                linearTimeScale: ACTION_TIMESCALE
+            });
+        },
 
-                    Entities.updateAction(stickData.ballID, stickData.actionID, {
-                        pointToOffsetFrom: stickProps.position,
-                        linearDistance: TETHER_LENGTH,
-                        linearTimeScale: TETHER_TIMESCALE
-                    });
+        hasAction: function() {
+            var actionProps = Entities.getActionArguments(this.ballID, this.actionID);
+            return actionProps.tag == ACTION_TAG;
+        },
 
-                    var ballProps = Entities.getEntityProperties(stickData.ballID);
-                    var linePoints = [];
-                    var normals = [];
-                    var strokeWidths = [];
-                    linePoints.push(Vec3.ZERO);
-                    normals.push(Vec3.multiplyQbyV(Camera.getOrientation(), Vec3.UNIT_NEG_Z));
-                    strokeWidths.push(TETHER_WIDTH);
-                    linePoints.push(Vec3.subtract(ballProps.position, stickProps.position));
-                    normals.push(Vec3.multiplyQbyV(Camera.getOrientation(), Vec3.UNIT_NEG_Z));
-                    strokeWidths.push(TETHER_WIDTH);
+        hasRequiredComponents: function() {
+            return this.hasBall() && this.hasAction() && this.hasLine();
+        },
 
-                    Entities.editEntity(stickData.lineID, {
-                        linePoints: linePoints,
-                        normals: normals,
-                        strokeWidths: strokeWidths,
-                        position: stickProps.position
-                    });
-                } catch (e) {
-                }
-            }
+        repositionAction: function() {
+            var stickProps = Entities.getEntityProperties(this.entityID);
+
+            Entities.updateAction(this.ballID, this.actionID, {
+                pointToOffsetFrom: stickProps.position,
+                linearDistance: this.lineLength,
+                linearTimeScale: ACTION_TIMESCALE
+            });
+
+            var ballProps = Entities.getEntityProperties(this.ballID);
+            var linePoints = [];
+            var normals = [];
+            var strokeWidths = [];
+            linePoints.push(Vec3.ZERO);
+            normals.push(Vec3.multiplyQbyV(Camera.getOrientation(), Vec3.UNIT_NEG_Z));
+            strokeWidths.push(LINE_WIDTH);
+            linePoints.push(Vec3.subtract(ballProps.position, stickProps.position));
+            normals.push(Vec3.multiplyQbyV(Camera.getOrientation(), Vec3.UNIT_NEG_Z));
+            strokeWidths.push(LINE_WIDTH);
+
+            var lineProps = Entities.getEntityProperties(this.lineID);
+            Entities.editEntity(this.lineID, {
+                linePoints: linePoints,
+                normals: normals,
+                strokeWidths: strokeWidths,
+                position: stickProps.position,
+                lifetime: Math.round(lineProps.age + 2)
+            });
         }
     };
 
+    // entity scripts should return a newly constructed object of our type
     return new tetherballStick();
 });
